0d3a1de2562a635580a8980210108a86a7df7291
4wip <4wip@4wip.com>
2025-08-01 18:20:20 +0200

    embedbuilder manquant

--- Commands/Utilitaires/snipe.js
+++ Commands/Utilitaires/snipe.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 const db = require('../../Events/loadDatabase');
 const config = require('../../config.json');
 
